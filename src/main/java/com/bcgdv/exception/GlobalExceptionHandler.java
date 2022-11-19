541ad210f1bd0677a2ff9825db25d5319c07e2f9
Badr Kacimi <badr.kacimi@tui.com>
2022-11-19 17:01:03 +0100

    Code cleanup

--- src/main/java/com/bcgdv/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bcgdv/exception/GlobalExceptionHandler.java
@@ -6,8 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.ArrayList;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -15,12 +13,6 @@ public class GlobalExceptionHandler {
             = {CreateOrderHandler.class })
     protected ResponseEntity<Response<Object>> handleCreateOrderException(CreateOrderHandler exception) {
         return new ResponseEntity<>(new Response<>(false,500,null,exception.getMessage()), null, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @ExceptionHandler(value
-            = {OrderNotFoundHandler.class })
-    protected ResponseEntity<Response<Object>> handleOrderNotFoundException(OrderNotFoundHandler exception) {
-        return new ResponseEntity<>(new Response<>(false,404,new ArrayList<>(),exception.getMessage()), null, HttpStatus.NOT_FOUND);
     }
 
 }
--- src/test/java/com/bcgdv/WatchApiSimpleApplicationTests.java
+++ src/test/java/com/bcgdv/WatchApiSimpleApplicationTests.java
@@ -8,6 +8,7 @@ class WatchApiSimpleApplicationTests {
 
   @Test
   void contextLoads() {
+
   }
 
 }

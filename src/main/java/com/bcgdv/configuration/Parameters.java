03cbfbbbc3bb1e11df4b560a2f61289902414778
Badr Kacimi <badr.kacimi@tui.com>
2022-11-19 02:16:42 +0100

    finish unit/integration tests

--- src/main/java/com/bcgdv/configuration/Parameters.java
+++ src/main/java/com/bcgdv/configuration/Parameters.java
@@ -8,18 +8,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties("com.bcgdv")
 public class Parameters {
+
     private Double rolexPrice;
+    private int rolexDiscountPrice;
+    private int rolexDiscountQuantity;
+
     private Double michaelkorsPrice;
+    private int michaelkorsDiscountPrice;
+    private int michaelkorsDiscountQuantity;
+
     private Double swatchPrice;
     private Double casioPrice;
+
     private String rolexID;
     private String michaelkorsID;
     private String swatchID;
     private String casioID;
-    private int rolexDiscountPrice;
-    private int michaelkorsDiscountPrice;
-    private int rolexDiscountQuantity;
-    private int michaelkorsDiscountQuantity;
 
 
 }
